801a4524533b70e1459489ca194ec8ff041cc466
Artem Olnev <artem_olnev@mail.ru>
2022-03-03 05:00:39 +0400

    Digital Design test

--- src/com/olnev/artem/ConvertToCron.java
+++ src/com/olnev/artem/ConvertToCron.java
@@ -2,31 +2,48 @@ package com.olnev.artem;
 
 import com.digdes.school.DatesToCronConvertException;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-//import java.util.Date;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 
 public class ConvertToCron {
-    public static void main(String[] args) {
+    public static String[] input = {
+            "2022-01-25T08:00:00",
+            "2022-01-25T08:30:00",
+            "2022-01-25T09:00:00",
+            "2022-01-25T09:30:00",
+            "2022-01-26T08:00:00",
+            "2022-01-26T08:30:00",
+            "2022-01-26T09:00:00",
+            "2022-01-26T09:30:00"};
+    public static Date[] dates = new Date[input.length];
+    public static Long[] longDates = new Long[input.length];
+
+    // HashMap, где key - разница между датами (в Long-формате), а value - как часто такая разница встречается
+    public static HashMap<Long, Integer> differences = new HashMap<>();
+
+    public static void main(String[] args) throws DatesToCronConvertException {
         getImplementationInfo();
+        convert(input);
+//        print();
     }
 
-//      Скоро здесь будет решение
+    // Скоро ниже будет решение
 
-//    public static String convert(java.util.Date[] dates) throws DatesToCronConvertException {
-//        SimpleDateFormat sdf = new SimpleDateFormat("ss-mm-hh-dd-MM-yyyy");
-//        String formatTimeString;
-//        try {
-//            if (dates != null) {
-//                for (java.util.Date date : dates) {
-//                    date = sdf.format(date);
-//                }
-//            }
-//        } catch (Exception e) {
-//            throw new DatesToCronConvertException();
-//        }
-//        return formatTimeString = "";
-//    }
+    // преобразование списка дат со временем в cron-формат
+    public static void convert(String[] input) throws DatesToCronConvertException {
+        stringsToDates(input);
+        datesToLong(dates);
+        Arrays.sort(longDates);
+        calculateDateDifferences(longDates);
+        requirement50percent();
+        cronCreation(); // не написан
+    }
 
+    // вывод информации о реализации интерфейса (ФИО, имя класса реализации, пакет, ссылка на github)
     public static void getImplementationInfo() {
         String devName = "Olnev Artem Igorevich";
         String githubLink = "https://github.com/kidwithbeard/digdes-feb-22.git";
@@ -35,8 +52,71 @@ public class ConvertToCron {
                 obj.getClass().getSimpleName() + "\n" +
                 obj.getClass().getPackageName() + "\n" +
                 githubLink);
+    }
 
-//        SimpleDateFormat sdf = new SimpleDateFormat("ss mm hh dd MM yyyy");
-//        System.out.println(sdf.format("2022-01-25T08:00:00"));
+    // парсинг строки в дату
+    public static Date parseStringToDate(String str) throws DatesToCronConvertException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
+        Date date;
+        try {
+            date = sdf.parse(str);
+        } catch (ParseException e) {
+            System.out.println("Parsing error");
+            throw new DatesToCronConvertException();
+        }
+        return date;
+    }
+
+    // преобразование массива строк в массив дат
+    public static void stringsToDates(String[] strings) throws DatesToCronConvertException {
+        for (int i = 0; i < strings.length; i++) {
+            dates[i] = parseStringToDate(strings[i]);
+        }
+    }
+
+    // преобразование массива дат в массив Long'ов
+    public static void datesToLong(Date[] dates) {
+        for (int i = 0; i < dates.length; i++) {
+            longDates[i] = dates[i].getTime();
+        }
+    }
+
+    // запись в HashMap разниц между датами
+    public static void calculateDateDifferences(Long[] longDates) {
+        if (longDates.length > 1) {
+            for (int i = 1; i < longDates.length; i++) {
+                long difference = dates[i].getTime() - dates[i - 1].getTime();
+                if (differences.containsKey(difference)) {
+                    differences.put(difference, differences.get(difference) + 1);
+                } else {
+                    differences.put(difference, 1);
+                }
+            }
+        }
+    }
+
+    // проверка, что cron удовлетворяет не меньше половины дат (> 50%)
+    public static void requirement50percent() throws DatesToCronConvertException {
+        if (Collections.max(differences.values()) < input.length / 2) {
+            throw new DatesToCronConvertException();
+        }
+    }
+
+    // формирование cron
+    public static void cronCreation() {
+
+    }
+
+    // метод для проверки
+    public static void print() throws DatesToCronConvertException {
+        stringsToDates(input);
+        for (Date date : dates) {
+            System.out.println(date);
+        }
+
+        for (HashMap.Entry<Long, Integer> item : differences.entrySet()) {
+
+            System.out.printf("Key: %s  Value: %s \n", item.getKey(), item.getValue());
+        }
     }
 }
